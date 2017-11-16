dfa72d57bdf2f339426851422cd2060433b34a15
Thibault Seisel <seisel.thibault@gmail.com>
2017-11-16 19:11:40 +0100

    Spek: tests for algorithms in Color.kt

--- library/src/test/kotlin/com/github/thibseisel/kdenticon/rendering/ColorSpek.kt
+++ library/src/test/kotlin/com/github/thibseisel/kdenticon/rendering/ColorSpek.kt
@@ -1,9 +1,11 @@
 package com.github.thibseisel.kdenticon.rendering
 
+import com.github.thibseisel.kdenticon.assertFailsWith
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 
 class ColorSpec : Spek({
 
@@ -49,7 +51,7 @@ class ColorSpec : Spek({
         }
     }
 
-    describe("the color channel extension properties") {
+    describe("The color channel extension properties") {
         val color = 0xABE0738C.toInt()
 
         it("should extract the alpha channel") {
@@ -66,6 +68,51 @@ class ColorSpec : Spek({
 
         it("should extract the blue channel") {
             assertEquals(0x8C, color.blue)
+        }
+    }
+
+    describe("The HSL to RGB color conversion algorithm") {
+
+        it("should fail with IllegalArgumentException if any parameter does not fit in 0..1") {
+            assertFailsWith<IllegalArgumentException> { colorFromHsl(1.2f, 0f, 0f) }
+            assertFailsWith<IllegalArgumentException> { colorFromHsl(0f, -3f, 0f) }
+            assertFailsWith<IllegalArgumentException> { colorFromHsl(0f, 0f, 16f) }
+        }
+
+        it("should return a shade of grey if saturation is 0") {
+            // Shades of grey have the same value for each color channel
+            val color = colorFromHsl(0.67f, 0f, 0.88f)
+            assertTrue(color.red == color.green)
+            assertTrue(color.green == color.blue)
+            // Implicit equality between red and blue channel values
+        }
+
+        it("should always return an opaque color") {
+            // Opaque colors have the maximum alpha value
+            assertEquals(0xFF, colorFromHsl(0f, 0f, 0f).alpha)
+            assertEquals(0xFF, colorFromHsl(1f, 1f, 1f).alpha)
+            assertEquals(0xFF, colorFromHsl(0.56f, 0.63f, 0.22f).alpha)
+        }
+
+        it("should translate to the color red") {
+            // In HSL, red is represented by the min and max hue (same point on the chromatic circle)
+            assertEquals(0xFFFF0000.toInt(), colorFromHsl(0f, 1f, 0.5f))
+            assertEquals(0xFFFF0000.toInt(), colorFromHsl(1f, 1f, 0.5f))
+        }
+
+        it("should translate to the color green") {
+            assertEquals(0xFF00FF00.toInt(), colorFromHsl(1 / 3f, 1f, 0.5f))
+        }
+
+        it("should translate to the color blue") {
+            assertEquals(0xFF0000FF.toInt(), colorFromHsl(2 / 3f, 1f, 0.5f))
+        }
+        it("should translate to black when lightness is 0") {
+            assertEquals(0xFF000000.toInt(), colorFromHsl(0.44f, 0.17f, 0f))
+        }
+
+        it("should translate to white when lightness is 1") {
+            assertEquals(0xFFFFFFFF.toInt(), colorFromHsl(0.44f, 0.17f, 1f))
         }
     }
 })
