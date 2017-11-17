0030246af5b44701362a9c3d0df7fcc35a4ab5c2
Thibault SEISEL <seisel.thibault@gmail.com>
2017-11-17 16:29:52 +0100

    Implementation of SvgRenderer

--- library/src/main/kotlin/com/github/thibseisel/kdenticon/rendering/SvgRenderer.kt
+++ library/src/main/kotlin/com/github/thibseisel/kdenticon/rendering/SvgRenderer.kt
@@ -1,20 +1,75 @@
 package com.github.thibseisel.kdenticon.rendering
 
-class SvgRenderer : Renderer() {
+import com.github.thibseisel.kdenticon.Colors
+import java.io.Writer
+import java.util.*
+
+/**
+ * A Renderer implementation that renders icons as SVG path instructions.
+ *
+ * @constructor Creates a new instance of a SvgRenderer.
+ * @param width The width of the icon in pixels.
+ * @param height The height of the icon in pixels.
+ */
+class SvgRenderer(
+        private val width: Int,
+        private val height: Int
+) : Renderer() {
+
+    private val pathsByColor = HashMap<Int, SvgPath>()
+
+    private lateinit var path: SvgPath
+    private var backgroundColor = Colors.TRANSPARENT
 
     override fun renderShape(color: Int, action: Runnable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        path = pathsByColor.getOrPut(color, ::SvgPath)
+        action.run()
     }
 
     override fun setBackground(color: Int) {
-        TODO("not implemented")
+        backgroundColor = color
     }
 
     override fun addCircleNoTransform(location: PointF, diameter: Float, counterClockwise: Boolean) {
-        TODO("not implemented")
+        path.addCircle(location, diameter, counterClockwise)
     }
 
     override fun addPolygonNoTransform(points: Array<PointF>) {
-        TODO("not implemented")
+        path.addPolygon(points)
+    }
+
+    /**
+     * Write SVG instructions with the specified Writer.
+     * @param writer The output writer to which the SVG will be written.
+     * @param partial If `true` an SVG string without the root svg tag will be rendered.
+     */
+    fun save(writer: Writer, partial: Boolean) {
+
+        // Add SVG root element tag if requested
+        if (!partial) {
+            writer.write("""
+                <svg xmlns="http://www.w3.org/2000/svg" width="$width" height="$height"
+                viewBox="0 0 $width $height" preserveAspectRatio="xMidYMid meet">
+                """.trimIndent())
+        }
+
+        // Draw the background only if it is not transparent
+        if (backgroundColor.alpha > 0) {
+            val opacity = backgroundColor.alpha / 255f
+            writer.write("""
+                <rect fill="${backgroundColor.toColorString()}" fill-opacity="$opacity"
+                x="0" y="0" width="$width" height="$height" />
+            """.trimIndent())
+        }
+
+        // Define each shape as an SVG path
+        for (path in pathsByColor.values) {
+            writer.write("""<path fill="${backgroundColor.toColorString()}" d="$path" />""")
+        }
+
+        // Close the SVG root element if requested
+        if (!partial) {
+            writer.write("</svg>")
+        }
     }
 }
